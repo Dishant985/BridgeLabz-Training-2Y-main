9b0cf6653216e78cbcdfae98c0b479aa690790d5
SHAURYA SINGHAL <shauryasinghal150@gmail.com>
2025-08-19 10:55:07 +0530

    Added harry.java program

---
+++ harry.java
@@ -0,0 +1,8 @@
+public class harry {
+    public static void main(String[] args) {
+        int birth= 2000,current=2025,x;
+        x= current-birth;
+        System.out.println("age: " + x);
+    }
+    
+}
